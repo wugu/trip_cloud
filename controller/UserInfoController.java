606527cb672eaa7dd7fb575ae4ed85480f0be029
wugu <110274235+wugu@users.noreply.github.com>
2024-05-18 22:57:22 +0800

    Add files via upload

---
+++ controller/UserInfoController.java
@@ -0,0 +1,45 @@
+package com.pzhu.user.controller;
+
+import com.pzhu.core.utils.R;
+import com.pzhu.user.domain.UserInfo;
+import com.pzhu.user.service.UserInfoService;
+import com.pzhu.user.vo.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserInfoController {
+    /*@Autowired
+    private UserInfoService userInfoService;*/
+
+//  这种方法是构造器注入，好处是可以有爆红，在有空指针异常时
+    private final UserInfoService userInfoService;
+
+    public UserInfoController(UserInfoService userInfoService){
+        this.userInfoService = userInfoService;
+    }
+
+    @GetMapping
+    public List<UserInfo> test(){
+        List<UserInfo> userInfos = userInfoService.list();
+        return userInfos;
+    }
+
+    @GetMapping("/phone/exists")
+    public R<Boolean> checkPhoneExists(String phone){//返回只需要存在或者不存在手机号，true/false
+        return R.success(userInfoService.findByPhone(phone) != null);
+    }
+
+    @PostMapping("/register")
+    public R<?> register(RegisterRequest req){//泛型问号表示返回数据无所谓
+        userInfoService.register(req);
+        return R.success();
+    }
+
+}
